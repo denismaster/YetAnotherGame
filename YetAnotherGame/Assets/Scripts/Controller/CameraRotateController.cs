c5d29e314e81adfaf1c53e8415a75b7fa3733d8f
justgoodwin <saffer1234@mail.ru>
2019-01-05 14:59:55 +0300

    добавил вращенеи камеры

---
+++ YetAnotherGame/Assets/Scripts/Controller/CameraRotateController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CameraRotateController : MonoBehaviour
+{
+    public float speed = 10f;
+    public float rotationSpeed = 100f;
+    private float moveVertical;
+    private float rotateHorizontal;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get axis angle
+        moveVertical = Input.GetAxis("Mouse Y") * speed;
+        rotateHorizontal = Input.GetAxis("Mouse X") * rotationSpeed;
+
+        // Make it move 10 meters per second instead of 10 meters per frame
+        moveVertical *= Time.deltaTime;
+        rotateHorizontal *= Time.deltaTime;
+
+        //// Move translation along the object's z-axis
+        //transform.Translate(0, 0, moveVertical);
+
+        transform.Rotate(moveVertical, rotateHorizontal, 0);
+    }
+}
