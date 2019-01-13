d31faaa69f02dd7e06bc16f049f85b6308166866
justgoodwin <saffer1234@mail.ru>
2019-01-13 20:48:52 +0300

    #16: added camera rotation via mouse

--- YetAnotherGame/Assets/Scripts/Constants.cs
+++ YetAnotherGame/Assets/Scripts/Constants.cs
@@ -6,4 +6,11 @@ public class Constants
 {
     //button name for change camera point of view
     public const string ChangeView = "ChangeView";
+
+    public const string MouseX = "Mouse X";
+
+    public const string MouseY = "Mouse Y";
+
+    public const string Player = "Player";
+    public const string FakeHead = "FakeHead";
 }
