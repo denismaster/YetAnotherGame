8ff1161192240fedbf9f2a6bccdcff784cdd74fe
minozemtsev <minozemtsev@xrm.ru>
2019-01-26 16:33:40 +0300

    #3: remove "m" prefix from private fields, get rid of errors in editor caused by SimpleAnimations.controller

--- YetAnotherGame/Assets/Scripts/Controllers/PlayerController.cs
+++ YetAnotherGame/Assets/Scripts/Controllers/PlayerController.cs
@@ -3,27 +3,27 @@ using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float m_moveSpeed = 2;
-    [SerializeField] private float m_turnSpeed = 150;
-    [SerializeField] private float m_jumpForce = 4;
-    [SerializeField] private Animator m_animator = null;
-    [SerializeField] private Rigidbody m_rigidBody = null;
+    [SerializeField] private float _moveSpeed = 2;
+    [SerializeField] private float _turnSpeed = 150;
+    [SerializeField] private float _jumpForce = 4;
+    [SerializeField] private Animator _animator = null;
+    [SerializeField] private Rigidbody _rigidBody = null;
 
-    private float m_currentV = 0;
-    private float m_currentH = 0;
+    private float _currentV = 0;
+    private float _currentH = 0;
 
-    private readonly float m_interpolation = 10;
-    private readonly float m_walkScale = 0.33f;
-    private readonly float m_backwardsWalkScale = 0.16f;
-    private readonly float m_backwardRunScale = 0.66f;
-    private float m_jumpTimeStamp = 0;
-    private float m_minJumpInterval = 0.25f;
+    private readonly float _interpolation = 10;
+    private readonly float _walkScale = 0.33f;
+    private readonly float _backwardsWalkScale = 0.16f;
+    private readonly float _backwardRunScale = 0.66f;
+    private float _jumpTimeStamp = 0;
+    private float _minJumpInterval = 0.25f;
 
-    private bool m_wasGrounded;
-    private bool m_isGrounded;
+    private bool _wasGrounded;
+    private bool _isGrounded;
 
-    private Vector3 m_currentDirection = Vector3.zero;
-    private List<Collider> m_collisions = new List<Collider>();
+    private Vector3 _currentDirection = Vector3.zero;
+    private List<Collider> _collisions = new List<Collider>();
 
     private void Start()
     {
@@ -37,10 +37,10 @@ public class PlayerController : MonoBehaviour
         {
             if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > 0.5f)
             {
-                if (!m_collisions.Contains(collision.collider)) {
-                    m_collisions.Add(collision.collider);
+                if (!_collisions.Contains(collision.collider)) {
+                    _collisions.Add(collision.collider);
                 }
-                m_isGrounded = true;
+                _isGrounded = true;
             }
         }
     }
@@ -53,47 +53,47 @@ public class PlayerController : MonoBehaviour
         {
             if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > 0.5f)
             {
-                validSurfaceNormal = true;                
+                validSurfaceNormal = true;
                 break;
             }
         }
 
         if(validSurfaceNormal)
         {
-            m_isGrounded = true;
-            if (!m_collisions.Contains(collision.collider))
+            _isGrounded = true;
+            if (!_collisions.Contains(collision.collider))
             {
-                m_collisions.Add(collision.collider);
+                _collisions.Add(collision.collider);
             }
         } 
         else
         {
-            if (m_collisions.Contains(collision.collider))
+            if (_collisions.Contains(collision.collider))
             {
-                m_collisions.Remove(collision.collider);
+                _collisions.Remove(collision.collider);
             }
-            if (m_collisions.Count == 0) 
+            if (_collisions.Count == 0) 
             { 
-                m_isGrounded = false; 
+                _isGrounded = false; 
             }
         }
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        if(m_collisions.Contains(collision.collider))
+        if(_collisions.Contains(collision.collider))
         {
-            m_collisions.Remove(collision.collider);
+            _collisions.Remove(collision.collider);
         }
-        if (m_collisions.Count == 0)
+        if (_collisions.Count == 0)
         { 
-            m_isGrounded = false; 
+            _isGrounded = false; 
         }
     }
 
 	void Update ()
     {
-        m_animator.SetBool("Grounded", m_isGrounded);
+        _animator.SetBool("Grounded", _isGrounded);
 
         float v = Input.GetAxis("Vertical");
         float h = Input.GetAxis("Horizontal");
@@ -102,44 +102,44 @@ public class PlayerController : MonoBehaviour
 
         if (v < 0) 
         {
-            v *= walk ? m_backwardsWalkScale : m_backwardRunScale;
+            v *= walk ? _backwardsWalkScale : _backwardRunScale;
         } 
         else if (walk)
         {
-            v *= m_walkScale;
+            v *= _walkScale;
         }
 
-        m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
-        m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
+        _currentV = Mathf.Lerp(_currentV, v, Time.deltaTime * _interpolation);
+        _currentH = Mathf.Lerp(_currentH, h, Time.deltaTime * _interpolation);
 
-        transform.position += transform.forward * m_currentV * m_moveSpeed * Time.deltaTime;
-        transform.Rotate(0, m_currentH * m_turnSpeed * Time.deltaTime, 0);
+        transform.position += transform.forward * _currentV * _moveSpeed * Time.deltaTime;
+        transform.Rotate(0, _currentH * _turnSpeed * Time.deltaTime, 0);
 
-        m_animator.SetFloat("MoveSpeed", m_currentV);
+        _animator.SetFloat("MoveSpeed", _currentV);
 
         JumpingAndLanding();
 
-        m_wasGrounded = m_isGrounded;
+        _wasGrounded = _isGrounded;
     }
 
     private void JumpingAndLanding()
     {
-        bool jumpCooldownOver = (Time.time - m_jumpTimeStamp) >= m_minJumpInterval;
+        bool jumpCooldownOver = (Time.time - _jumpTimeStamp) >= _minJumpInterval;
 
-        if (jumpCooldownOver && m_isGrounded && Input.GetKey(KeyCode.Space))
+        if (jumpCooldownOver && _isGrounded && Input.GetKey(KeyCode.Space))
         {
-            m_jumpTimeStamp = Time.time;
-            m_rigidBody.AddForce(Vector3.up * m_jumpForce, ForceMode.Impulse);
+            _jumpTimeStamp = Time.time;
+            _rigidBody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
         }
 
-        if (!m_wasGrounded && m_isGrounded)
+        if (!_wasGrounded && _isGrounded)
         {
-            m_animator.SetTrigger("Land");
+            _animator.SetTrigger("Land");
         }
 
-        if (!m_isGrounded && m_wasGrounded)
+        if (!_isGrounded && _wasGrounded)
         {
-            m_animator.SetTrigger("Jump");
+            _animator.SetTrigger("Jump");
         }
     }
 }
