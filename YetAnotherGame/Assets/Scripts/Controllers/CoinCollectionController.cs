8219705d18c9aa91ed0bb0ee8e5d80aa1cc2e1c4
minozemtsev <minozemtsev@xrm.ru>
2019-02-03 17:40:16 +0300

    #27: add background music, coin collection sound, generate coin if player collect some of them

---
+++ YetAnotherGame/Assets/Scripts/Controllers/CoinCollectionController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CoinCollectionController : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (gameObject.tag == TagConstants.Coin && collision.gameObject.tag == TagConstants.Player)
+        {
+            SoundManagerController.Play(SoundConstants.CoinCollect);
+            Destroy(this.gameObject);
+        }
+    }
+}
