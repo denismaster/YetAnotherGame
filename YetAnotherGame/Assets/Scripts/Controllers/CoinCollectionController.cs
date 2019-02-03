8ec3a022682a5e3e87cbde376f779040283c3e85
denismaster <denismaster@outlook.com>
2019-02-03 18:47:10 +0300

    #12: Fix compilation issue

---
+++ YetAnotherGame/Assets/Scripts/Controllers/CoinCollectionController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CoinCollectionController : MonoBehaviour
+{
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (gameObject.tag == TagConstants.Coin && collision.gameObject.tag == TagConstants.Player)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
