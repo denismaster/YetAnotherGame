346a669827a1f0a23ea24aefa6337463ab711f98
justgoodwin <saffer1234@mail.ru>
2019-02-16 15:14:52 +0300

    #25: added maze generator, hide floor cells

---
+++ YetAnotherGame/Assets/Scripts/MazeGenerator/MazeManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MazeManager : MonoBehaviour {
+
+	public Maze mazePrefab;
+
+	private Maze mazeInstance;
+
+
+	private void Start () {
+		BeginGame();
+	}
+	
+
+
+	private void BeginGame () {
+		mazeInstance = Instantiate(mazePrefab) as Maze;
+		
+		mazeInstance.Generate();
+		// playerInstance = Instantiate(playerPrefab) as Player;
+		// playerInstance.SetLocation(mazeInstance.GetCell(mazeInstance.RandomCoordinates));
+	}
+
+}
